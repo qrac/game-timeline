83b14d542ea48bc9f9474eb5641130151674faee
Qrac <qrac.jp@gmail.com>
2025-07-20 18:11:52 +0900

    Fix style

--- src/components/timeline/index.tsx
+++ src/components/timeline/index.tsx
@@ -126,52 +126,66 @@ export function ComponentTimeline({
         //ref={ref}
       >
         {filteredItemList.length > 0 ? (
-          <div className="timeline-contents">
-            <div className="timeline-bar" />
-            <div className="timeline-years">
-              {visibleYearList.map((year) => {
-                const mainItemList = filteredItemList
-                  .filter((item) => {
-                    return item.year === year && item.category !== "news"
-                  })
-                  .sort((a, b) => a.timestamp - b.timestamp)
-                const subItemList = filteredItemList
-                  .filter((item) => {
-                    return item.year === year && item.category === "news"
-                  })
-                  .sort((a, b) => a.timestamp - b.timestamp)
-                const emptyMain = mainItemList.length === 0
-                const emptySub = subItemList.length === 0
-                const emptyItems = emptyMain && emptySub
+          <div className="timeline-years">
+            {visibleYearList.map((year) => {
+              const mainItemList = filteredItemList
+                .filter((item) => {
+                  return item.year === year && item.category !== "news"
+                })
+                .sort((a, b) => a.timestamp - b.timestamp)
+              const subItemList = filteredItemList
+                .filter((item) => {
+                  return item.year === year && item.category === "news"
+                })
+                .sort((a, b) => a.timestamp - b.timestamp)
+              const emptyMain = mainItemList.length === 0
+              const emptySub = subItemList.length === 0
+              const emptyItems = emptyMain && emptySub
 
-                if (emptyItems && omitEmptyYears) {
-                  return null
-                }
-                return (
-                  <div className="timeline-year" key={year}>
-                    <h2
-                      className="timeline-year-title"
-                      id={year.toString()}
-                      ref={(el) => {
-                        if (el) yearRefs.current.set(year, el)
-                      }}
+              if (emptyItems && omitEmptyYears) {
+                return null
+              }
+              return (
+                <div className="timeline-year" key={year}>
+                  <h2
+                    className="timeline-year-title"
+                    id={year.toString()}
+                    ref={(el) => {
+                      if (el) yearRefs.current.set(year, el)
+                    }}
+                  >
+                    <span className="timeline-year-title-text">{year}</span>
+                  </h2>
+                  <div className="timeline-year-bar" />
+                  {!emptyItems && (
+                    <div
+                      className={clsx(
+                        "timeline-year-columns",
+                        emptySub && "is-empty-sub"
+                      )}
                     >
-                      <span className="timeline-year-title-text">{year}</span>
-                    </h2>
-                    {!emptyItems && (
                       <div
                         className={clsx(
-                          "timeline-year-columns",
-                          emptySub && "is-empty-sub"
+                          "timeline-year-column is-main",
+                          mainItemList.length >= 2 && "is-separate"
                         )}
                       >
+                        {mainItemList.map((item) => (
+                          <ComponentItem
+                            key={item.name}
+                            item={item}
+                            tagList={tagList}
+                          />
+                        ))}
+                      </div>
+                      {subItemList.length > 0 && (
                         <div
                           className={clsx(
-                            "timeline-year-column is-main",
-                            mainItemList.length >= 2 && "is-separate"
+                            "timeline-year-column is-sub",
+                            subItemList.length >= 2 && "is-separate"
                           )}
                         >
-                          {mainItemList.map((item) => (
+                          {subItemList.map((item) => (
                             <ComponentItem
                               key={item.name}
                               item={item}
@@ -179,28 +193,12 @@ export function ComponentTimeline({
                             />
                           ))}
                         </div>
-                        {subItemList.length > 0 && (
-                          <div
-                            className={clsx(
-                              "timeline-year-column is-sub",
-                              subItemList.length >= 2 && "is-separate"
-                            )}
-                          >
-                            {subItemList.map((item) => (
-                              <ComponentItem
-                                key={item.name}
-                                item={item}
-                                tagList={tagList}
-                              />
-                            ))}
-                          </div>
-                        )}
-                      </div>
-                    )}
-                  </div>
-                )
-              })}
-            </div>
+                      )}
+                    </div>
+                  )}
+                </div>
+              )
+            })}
           </div>
         ) : (
           <div className="timeline-blank">表示するデータがないよ！</div>
