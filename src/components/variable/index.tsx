073b1e5dabd26a1f96d708329d54aa825d64b651
Qrac <qrac.jp@gmail.com>
2025-08-12 23:11:28 +0900

    Fix id scroll

--- src/components/variable/index.tsx
+++ src/components/variable/index.tsx
@@ -1,17 +1,19 @@
 import type { Setting } from "../../types"
 
 export function ComponentVariable({ setting }: { setting: Setting }) {
-  const { colorList, scrollbarWidth } = setting
+  const { colorList, scrollbarWidth, headerHeight } = setting
   const colorCss = colorList
     .map((color) => {
       return `.is-accent-${color.id} { --pj-color-accent: ${color.color}; }`
     })
     .join("\n")
   const scrollbarCss = `:root { --pj-scrollbar-width: ${scrollbarWidth}px; }`
+  const headerCss = `:root { --pj-fixed-header-height: ${headerHeight}px; }`
   return (
     <>
       {colorList.length > 0 && <style>{colorCss}</style>}
       {scrollbarWidth > 0 && <style>{scrollbarCss}</style>}
+      <style>{headerCss}</style>
     </>
   )
 }
